b0af8cabc361cbe3b8800334b9ddde3b8e5f9e96
Khathu Makatu <kha2maka2@gmail.com>
2024-03-19 12:55:23 +0200

    Add project files.

---
+++ Regex C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net; 
+using System.Text.RegularExpressions; 
+
+//Purpose: Used for regular expressions/ Pattern replacement of code
+class Program
+{
+	public static void Main(string[] args)
+	{
+	
+		string regexPattern = @"<!DOCTYPE html>";	// Pattern identified
+		string replacement = @"TestSuccessful";		// Replacement of pattern 
+
+		// Test File 
+		string filePath = @"C:\Users\Khathutshelo Makatu\Downloads\temp\AddField.asp";
+		string fileContent = File.ReadAllText(filePath);
+
+		// Replace pattern with replace
+		fileContent = Regex.Replace(fileContent, regexPattern, replacement);
+
+		// Update and Save the file
+		File.WriteAllText(filePath, fileContent);
+
+		// Display update in console
+		Console.WriteLine(fileContent); 
+	}
+}
+
